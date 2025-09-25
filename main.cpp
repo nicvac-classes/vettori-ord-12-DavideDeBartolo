d9675b6887e77c772cdce60d30a4eb25b6c14249
DavideDeBartolo <davide.debartolo@marconibari.edu.it>
2025-09-25 11:24:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int N;
+    std::cout << "Inserisci il numero di cifre da generare: ";
+    std::cin >> N;
+
+    std::vector<int> R(N);
+    std::srand(std::time(0));
+
+    for (int i = 0; i < N; ++i) {
+        R[i] = std::rand() % (N * 10 + 1);
+    }
+
+    std::sort(R.begin(), R.end());
+
+    std::cout << "Vettore R ordinato: ";
+    for (const auto& num : R) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
